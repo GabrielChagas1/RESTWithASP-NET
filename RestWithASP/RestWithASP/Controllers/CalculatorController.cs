9ac007c2aa667f9fdbeda8679e45a496ae072a6f
Gabriel Chagas <gabrielserq@outlook.com>
2020-01-06 10:54:05 -0300

    add sub, mult and root at calculator

--- RestWithASP/RestWithASP/Controllers/CalculatorController.cs
+++ RestWithASP/RestWithASP/Controllers/CalculatorController.cs
@@ -11,7 +11,7 @@ namespace RestWithASP.Controllers
     public class CalculatorController : Controller
     {
         // GET api/values/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public ActionResult<string> Sum(string firstNumber, string secondNumber)
         {
            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
@@ -19,9 +19,61 @@ namespace RestWithASP.Controllers
                 var Sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(Sum.ToString());
            }
+           return BadRequest("Invalid Input");
 
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sub(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var Sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(Sub.ToString());
+            }
             return BadRequest("Invalid Input");
+        }
 
+        [HttpGet("mul/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mul(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var Mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(Mult.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var Div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(Div.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("root/{Number}")]
+        public ActionResult<string> Root(string Number)
+        {
+            if (IsNumeric(Number))
+            {
+                return Ok(Math.Sqrt(ConvertToDouble(Number)).ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //Métodos de conversões 
+        private Double ConvertToDouble(string Number)
+        {
+            if(Double.TryParse(Number, out Double Value))
+            {
+                return Value;
+            }
+            return 0;
         }
 
         private Decimal ConvertToDecimal(string Number)
