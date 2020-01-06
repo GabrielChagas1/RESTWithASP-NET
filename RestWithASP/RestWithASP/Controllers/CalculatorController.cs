41fabbd2e765f9e8bace3dcec4c78a646dc30479
Gabriel Chagas <gabrielserq@outlook.com>
2020-01-06 10:29:58 -0300

    creating sum calculator

---
+++ RestWithASP/RestWithASP/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DO.Validation.Codes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASP.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+           if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+           {
+                var Sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(Sum.ToString());
+           }
+
+            return BadRequest("Invalid Input");
+
+        }
+
+        private Decimal ConvertToDecimal(string Number)
+        {
+            decimal DecimalValue;
+            if (decimal.TryParse(Number, out DecimalValue))
+            {
+                return DecimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string StrNumber)
+        {
+            bool isNumber = double.TryParse(StrNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double Number);
+            return isNumber;
+        }
+    }
+}
